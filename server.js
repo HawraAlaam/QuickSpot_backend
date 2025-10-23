251b66c4139e20841a420893133e7923e28c519c
HawraAlaam <hawraalaam@gmail.com>
2025-10-23 10:43:07 +0300

    backend setup

---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require('express')
+const morhan = require('morgan')
+const cors = require('cors')
+
+
+const PORT = process.env.PORT || 3000
+
+const db = require('./db')
+
+const app = express()
+
+app.use(cors())
+app.use(morgan('dev'))
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+
+
+
+app.use('/', (req, res) => {
+  res.send(`Connected!`)
+})
+
+app.listen(PORT, () => {
+  console.log(`Running Express server on Port ${PORT} . . .`)
+})
