7d59f173bdbb988a9c219b14f9eed99aaa35382e
mohamed-janahi <Mohamed.janahi98@gmail.com>
2025-10-25 10:25:34 +0300

    finished all backend for job

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express')
-const morhan = require('morgan')
+const morgan = require('morgan')
 const cors = require('cors')
+
 
 
 const PORT = process.env.PORT || 3000
@@ -14,7 +15,8 @@ app.use(morgan('dev'))
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 
-
+const jobRouter = require ("./routes/jobRouter.js")
+app.use('/jobs', jobRouter)
 
 app.use('/', (req, res) => {
   res.send(`Connected!`)
